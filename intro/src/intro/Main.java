a1732d96c8e54d54fdc41b3487d678194a47e8d6
oguza <oguza@192.168.0.9>
2024-11-13 16:06:31 +0300

    Deneme

---
+++ intro/src/intro/Main.java
@@ -0,0 +1,23 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] list = { "asdasda", "asdasd", "wewqeq", "asdasdewvxvxcv" };
+		deneme(list);
+		var asd = 1;
+		var sasd = "asdasd";
+		var sas1d = "asdasd";
+
+	}
+
+	public static void deneme(String[] iterable) {
+
+		for (String asd : iterable) {
+			System.out.println(asd);
+		}
+	}
+
+}
